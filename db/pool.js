d74f95e3936311d4e3ea3d450751450eecddbb0b
Ahmad Nourallah <ahmadnurallah@gmail.com>
2025-04-27 08:15:18 -0400

    Add a module to establish database connection

---
+++ db/pool.js
@@ -0,0 +1,5 @@
+const { Pool } = require('pg');
+
+module.exports = new Pool({
+    connectionString: `postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
+});
